ead0237fb158b0741b70ef87204e6bef6e924cdb
yoyraso <yoyoraso@gmail.com>
2020-03-27 21:31:19 +0200

    Python_Lab1

---
+++ problem3_lab1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter The Number :"))      #Input
+if(a>17):                               #checking the number to 17
+    x=abs(17-a)                         # number > 17
+else:
+    x=17-a                              # number < 17
+
+print(x)                                # printing the differance
