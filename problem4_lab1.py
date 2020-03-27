f863d70ee07a997b6880e59abd00308c7f353e73
yoyraso <yoyoraso@gmail.com>
2020-03-27 21:32:25 +0200

    Python_Lab1

---
+++ problem4_lab1.py
@@ -0,0 +1,9 @@
+a=int(input("Enter The First Number : "))
+b=int(input("Enter The First Number : "))
+c=int(input("Enter The First Number : "))
+if(a==b&a==c):
+    x=3*(a+b+c)
+else:
+    x=a+b+c
+
+print(x)
---
+++ problem6_lab1.py
@@ -0,0 +1,4 @@
+a=input("Enter Your Name : ")
+for char in range(len(a) - 1, -1, -1):
+  print(a[char], end="")
+print("\n")
